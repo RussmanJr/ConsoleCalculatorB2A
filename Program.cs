1c801120faa21f0ffa495897700a8325e3d68d09
Russell Sacobie <russell.sacobie@gmail.com>
2025-01-07 17:47:50 -0400

    Initial commit of project files
    Initial commit of project files

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleCalculatorB2A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcMethod mathMethod = new CalcMethod();
+            mathMethod.StartCalculator();
+        }
+    }
+}
